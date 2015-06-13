cb67a7b819439c0bc1cf4b6285168697b3dd70cb
hoangnh11 <hoangnh11@FSU1-HoangNH11-LA>
2015-06-13 18:16:24 +0700

    Create layout for tuyen & khach hang screen

---
+++ ViviProject/src/com/viviproject/ProfitReportActivity.java
@@ -0,0 +1,71 @@
+package com.viviproject;
+
+import com.viviproject.ultilities.Logger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
+
+public class ProfitReportActivity extends Activity implements OnClickListener{
+
+	private LinearLayout linBack, linSearch, linUpdate, linRefresh;
+	private TextView tvHeader;
+	private RadioGroup radioGroupDay;
+	private RadioButton radioDay;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profit_report_layout);
+		initLayout();
+	}
+	
+	public void initLayout(){
+		linBack = (LinearLayout) findViewById(R.id.linBack);
+		linBack.setOnClickListener(this);
+		linBack.setVisibility(View.VISIBLE);
+		
+		tvHeader = (TextView) findViewById(R.id.tvHeader);
+		tvHeader.setText(getResources().getString(R.string.PROFIT));
+		tvHeader.setVisibility(View.VISIBLE);
+		
+		linSearch = (LinearLayout) findViewById(R.id.linSearch);
+		linSearch.setOnClickListener(this);	
+		linSearch.setVisibility(View.VISIBLE);
+		
+		linUpdate = (LinearLayout) findViewById(R.id.linUpdate);
+		linUpdate.setOnClickListener(this);
+		linUpdate.setVisibility(View.VISIBLE);
+		
+		linRefresh = (LinearLayout) findViewById(R.id.linRefresh);
+		linRefresh.setOnClickListener(this);
+		
+		radioGroupDay = (RadioGroup) findViewById(R.id.radioGroupDay);
+		radioGroupDay.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				radioDay = (RadioButton) findViewById(checkedId);				
+			}
+		});
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.linBack:
+			finish();
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
