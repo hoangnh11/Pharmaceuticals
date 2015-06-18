5e3f9808e629f8285a68da06ff1a3e05ff1b4d3e
Do Thin <mr.dothin@gmail.com>
2015-06-18 11:43:05 +0700

    update layout

--- ViviProject/src/com/viviproject/projection/AcCompany.java
+++ ViviProject/src/com/viviproject/projection/AcCompany.java
@@ -1,21 +1,28 @@
 package com.viviproject.projection;
 
-import com.viviproject.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.viviproject.R;
 
 public class AcCompany  extends Activity implements OnClickListener{
 	private LinearLayout linBack;
 	private TextView tvHeader;
 	private LinearLayout linOptionSearch, linOptionFilter, linOptionRefresh;
 	private LinearLayout linButonSuMenhTamNhin, linButonLichSuPhatTrien, linButonChienLuocKinhDoanh;
+	private LinearLayout linLichSuPhatTrienBody;
 	private ImageView imgSuMenhTamNhinBody, imgLichSuPhatTrieBody, imgChienLuocKinhDoanh; 
+	private ScrollView scrollViewCompany;
+	
+	private boolean checkScrollBottom = false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -45,12 +52,15 @@ public class AcCompany  extends Activity implements OnClickListener{
 		linOptionRefresh = (LinearLayout) findViewById(R.id.linRefresh);
 		linOptionRefresh.setVisibility(View.VISIBLE);
 		
+		scrollViewCompany = (ScrollView) findViewById(R.id.scrollViewCompany);
+		
 		linButonSuMenhTamNhin = (LinearLayout) findViewById(R.id.linButonSuMenhTamNhin);
 		linButonSuMenhTamNhin.setOnClickListener(this);
 		imgSuMenhTamNhinBody = (ImageView) findViewById(R.id.imgSuMenhTamNhinBody);
 		
 		linButonLichSuPhatTrien = (LinearLayout) findViewById(R.id.linButonLichSuPhatTrien);
 		linButonLichSuPhatTrien.setOnClickListener(this);
+		linLichSuPhatTrienBody =(LinearLayout) findViewById(R.id.linLichSuPhatTrienBody);
 		imgLichSuPhatTrieBody = (ImageView) findViewById(R.id.imgLichSuPhatTrieBody);
 		
 		linButonChienLuocKinhDoanh = (LinearLayout) findViewById(R.id.linButonChienLuocKinhDoanh);
@@ -69,25 +79,58 @@ public class AcCompany  extends Activity implements OnClickListener{
 		case R.id.linButonSuMenhTamNhin:
 			if(imgSuMenhTamNhinBody.getVisibility() == View.VISIBLE){
 				imgSuMenhTamNhinBody.setVisibility(View.GONE);
+				linButonSuMenhTamNhin.setBackgroundResource(R.drawable.bg_button_expand_blue);
 			} else {
 				imgSuMenhTamNhinBody.setVisibility(View.VISIBLE);
+				linButonSuMenhTamNhin.setBackgroundResource(R.drawable.bg_button_expand_blue_press);
 			}
 			break;
 
 		case R.id.linButonLichSuPhatTrien:
 			if(imgLichSuPhatTrieBody.getVisibility() == View.VISIBLE){
 				imgLichSuPhatTrieBody.setVisibility(View.GONE);
+				linButonLichSuPhatTrien.setBackgroundResource(R.drawable.bg_button_expand_blue);
 			} else {
 				imgLichSuPhatTrieBody.setVisibility(View.VISIBLE);
+				linButonLichSuPhatTrien.setBackgroundResource(R.drawable.bg_button_expand_blue_press);
+				
+				/*scrollViewCompany.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+			        @Override
+			        public void onGlobalLayout() {
+			        	scrollViewCompany.post(new Runnable() {
+			                public void run() {
+			                	scrollViewCompany.scrollTo(0, linLichSuPhatTrienBody.getHeight()/10);
+			                }
+			            });
+			        }
+			    });*/
 			}
+			
 			break;
 			
 		case R.id.linButonChienLuocKinhDoanh:
+			checkScrollBottom = true;
 			if(imgChienLuocKinhDoanh.getVisibility() == View.VISIBLE){
 				imgChienLuocKinhDoanh.setVisibility(View.GONE);
+				linButonChienLuocKinhDoanh.setBackgroundResource(R.drawable.bg_button_expand_blue);
 			} else {
 				imgChienLuocKinhDoanh.setVisibility(View.VISIBLE);
+				linButonChienLuocKinhDoanh.setBackgroundResource(R.drawable.bg_button_expand_blue_press);
 			}
+			
+			scrollViewCompany.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+		        @Override
+		        public void onGlobalLayout() {
+		        	scrollViewCompany.post(new Runnable() {
+		                public void run() {
+		                	if (checkScrollBottom) {
+		                		scrollViewCompany.fullScroll(View.FOCUS_DOWN);
+		                		checkScrollBottom = false;
+		                	}
+		                }
+		            });
+		        }
+		    });
 			break;
 			
 		default:
