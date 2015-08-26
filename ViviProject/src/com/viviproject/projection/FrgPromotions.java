0f007ce420585d3faec0c7a8e619bd78a8b9bb64
Do Thin <mr.dothin@gmail.com>
2015-08-26 22:15:06 +0700

    update promotions

--- ViviProject/src/com/viviproject/projection/FrgPromotions.java
+++ ViviProject/src/com/viviproject/projection/FrgPromotions.java
@@ -2,12 +2,14 @@ package com.viviproject.projection;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
-import android.text.Html;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.viviproject.R;
 import com.viviproject.entities.EnDiscountProgramItem;
@@ -21,7 +23,7 @@ public class FrgPromotions extends FrgBaseFragmentPromotions {
 	public static FrgPromotions newInstance(Context context, EnDiscountProgramItem enDiscountProgram){
 		FrgPromotions f = new FrgPromotions();
 		f.enDiscountProgram = enDiscountProgram;
-		f.mContext = context;
+		f.setmContext(context);
 		return f;
 	}
 	
@@ -45,9 +47,17 @@ public class FrgPromotions extends FrgBaseFragmentPromotions {
     public void onAttach(Activity activity) {
     	// TODO Auto-generated method stub
     	super.onAttach(activity);
-    	this.mActivity = getActivity();
-    	this.mContext = getActivity();
+    	this.setmActivity(getActivity());
+    	this.setmContext(getActivity());
     }
+    
+    private class NewsWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
     
     /**
 	 * 
@@ -56,12 +66,52 @@ public class FrgPromotions extends FrgBaseFragmentPromotions {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		mView = inflater.inflate(R.layout.frg_promotions_layout, container, false);    	
-		TextView tvPromotionContents = (TextView) mView.findViewById(R.id.tvPromotionContents);
+		//TextView tvPromotionContents = (TextView) mView.findViewById(R.id.tvPromotionContents);
+		WebView webDetails = (WebView) mView.findViewById(R.id.webDetail);
+		if (Build.VERSION.SDK_INT >= 11) {
+			webDetails.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+		}
+		
+		webDetails.setBackgroundColor(0x00000000);
+		webDetails.setWebViewClient(new NewsWebViewClient());
+		webDetails.getSettings().setJavaScriptEnabled(true);
+		webDetails.getSettings().setSupportZoom(true);
+		webDetails.getSettings().setBuiltInZoomControls(true);
+		webDetails.getSettings().setLoadWithOverviewMode(true);
+		// these mode is default of android
+		webDetails.getSettings().setUseWideViewPort(false);
+		webDetails.getSettings().setDefaultTextEncodingName("utf-8");
+		webDetails.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+		webDetails.setScrollbarFadingEnabled(false);
 		
 		if(StringUtils.isNotBlank(enDiscountProgram.getContent())){
-			tvPromotionContents.setText(Html.fromHtml(enDiscountProgram.getContent()));
+			//tvPromotionContents.setText(Html.fromHtml(enDiscountProgram.getContent()));
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
+			    String base64 = Base64.encodeToString(enDiscountProgram.getContent().getBytes(), Base64.DEFAULT);
+			    webDetails.loadData(base64, "text/html; charset=utf-8", "base64");
+			} else {
+			    String header = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
+			    webDetails.loadData(header + enDiscountProgram.getContent(), "text/html; charset=UTF-8", null);
+
+			}
 		}
 		
 		return mView;
+	}
+
+	public Context getmContext() {
+		return mContext;
+	}
+
+	public void setmContext(Context mContext) {
+		this.mContext = mContext;
+	}
+
+	public Activity getmActivity() {
+		return mActivity;
+	}
+
+	public void setmActivity(Activity mActivity) {
+		this.mActivity = mActivity;
 	}
 }
